933294d55852e73132c7a5ab462998e089da294e
Amumug <fabriali4@gmail.com>
2024-10-28 12:54:41 +0100

    first-commit

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,11 +1,37 @@
 import React from "react";
 import { Button } from "./ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../components/ui/dropdown-menu";
 
 const Form = () => {
-  return <div>
+  const [position, setPosition] = React.useState("bottom");
 
-    <Button>SSSSSSSSSS</Button>
-  </div>;
+  return (
+    <div>
+      <Button variant="secondary">CLASS</Button>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline">Open</Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56 ">
+          <DropdownMenuLabel>Panel Position</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
+            <DropdownMenuRadioItem value="top">Top</DropdownMenuRadioItem>
+            <DropdownMenuRadioItem value="bottom">Bottom</DropdownMenuRadioItem>
+            <DropdownMenuRadioItem value="right">Right</DropdownMenuRadioItem>
+          </DropdownMenuRadioGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
 };
 
 export default Form;
